dd24f44e0f06b04f52a15a4a3dd58e3c79b81183
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2016-12-08 15:32:47 +0200

    add helper method, which prints all routes with URL and controller method invocation. Example: GET: api/Person/GetByName/{personName},   [FromUri]personName IEnumerable`1 PersonController.GetByName(personName)
    GET: api/Person,
    IEnumerable`1 PersonController.GetAll()
    
    GET: api/Person/{id},   [FromUri]id
    String PersonController.Get(id)
    
    POST: api/Person,   [FromBody]person
    Void PersonController.Post(person)
    
    PUT: api/Person/{id},   [FromUri]id, [FromBody]person
    Void PersonController.Put(id, person)
    
    DELETE: api/Person/{id},   [FromUri]id
    Void PersonController.Delete(id)

--- AddressBook.Ang/Global.asax.cs
+++ AddressBook.Ang/Global.asax.cs
@@ -1,5 +1,8 @@
 ﻿using System.Data.Entity;
+using System.Linq;
+using System.Text;
 using System.Web.Http;
+using System.Web.Http.Description;
 using System.Web.Mvc;
 using AddressBook.Core.DataAccess;
 
@@ -14,6 +17,24 @@ namespace AddressBook.Ang
       Database.SetInitializer(new AddressBookDbIfNotExistsInitializer());
       GlobalConfiguration.Configuration.DependencyResolver = new DefaultDependencyResolver(); // for ApiControllers
       DependencyResolver.SetResolver(new DefaultDependencyResolver()); // For MVC controllers
+
+      // print routes description. It helps to understand, which controller method is invoked
+      var routes = GetRoutesDescriptions(GlobalConfiguration.Configuration.Services.GetApiExplorer());
+    }
+
+    private static string GetRoutesDescriptions(IApiExplorer apiExplorer)
+    {
+      var sb = new StringBuilder();
+      foreach (var api in apiExplorer.ApiDescriptions)
+      {
+        var descriptor = (System.Web.Http.Controllers.ReflectedHttpActionDescriptor) api.ActionDescriptor;
+        var methodInfo = descriptor.MethodInfo;
+        var methodParameters = string.Join(", ", methodInfo.GetParameters().Select(p => p.Name));
+        sb.AppendLine($"{api.HttpMethod}: {api.RelativePath},   {string.Join(", ", api.ParameterDescriptions.Select(p => $"[{p.Source}]{p.Name}"))}");
+        sb.AppendLine($"{methodInfo.ReturnType.Name} {descriptor.ControllerDescriptor.ControllerType.Name}.{methodInfo.Name}({methodParameters})");
+        sb.AppendLine();
+      }
+      return sb.ToString();
     }
   }
 }
