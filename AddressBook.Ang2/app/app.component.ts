5ab444bae64917a74c69b3eb3f6f0cc5d0d4ddd2
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2017-02-23 11:44:28 +0200

    Extended example from https://metanit.com/web/angular2/1.2.php

--- AddressBook.Ang2/app/app.component.ts
+++ AddressBook.Ang2/app/app.component.ts
@@ -1,8 +1,37 @@
 ﻿import { Component } from "@angular/core";
+
+export class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number) {
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
+
 @Component({
-  selector: "my-app",
-  template: `<label>Введите имя:</label>
-             <input [(ngModel)]="name" placeholder="name">
-             <h1>Добро пожаловать {{name}}!</h1>`
+  selector: "purchase-app",
+  templateUrl: "./app/app.component.html"
 })
-export class AppComponent { name = ""; }
+export class AppComponent {
+
+  items: Item[] =
+  [
+    { purchase: "Хлеб", done: false, price: 15.9 },
+    { purchase: "Масло", done: false, price: 60 },
+    { purchase: "Картофель", done: true, price: 22.6 },
+    { purchase: "Сыр", done: false, price: 310 }
+  ];
+
+  addItem(text: string, price: number): void {
+
+    if (text == null || text == undefined || text.trim() === "")
+      return;
+    if (price == null || price == undefined)
+      return;
+
+    this.items.push(new Item(text, price));
+  }}
