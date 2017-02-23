8448d1423a7ee664edfb2cfebda92bda2d090e72
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2017-02-23 09:32:04 +0200

    Extended Angular example

--- AddressBook.Ang2/app/app.component.ts
+++ AddressBook.Ang2/app/app.component.ts
@@ -1,6 +1,8 @@
 ﻿import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
-  template: "<h1>My First Angular 2 App</h1>"
+  template: `<label>Введите имя:</label>
+             <input [(ngModel)]="name" placeholder="name">
+             <h1>Добро пожаловать {{name}}!</h1>`
 })
-export class AppComponent { }
+export class AppComponent { name = ""; }
